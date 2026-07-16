be7ad87ab6768edc203f03a4b4d8083b28e5c62e
Chanel AKOUEHOU <chanochanel70@gmail.com>
2026-07-16 10:03:00 +0100

    debug: enrichir le diagnostic storage Cloudinary

--- backend/PHAROS/urls.py
+++ backend/PHAROS/urls.py
@@ -6,10 +6,23 @@ from django.http import JsonResponse
 
 
 def _debug_storage(request):
+    from django.core.files.storage import default_storage
+    from hotels.models import Hotel
+    hotel = Hotel.objects.exclude(photo_principale='').filter(photo_principale__isnull=False).first()
+    exemple = None
+    if hotel and hotel.photo_principale:
+        exemple = {
+            'name_en_base': hotel.photo_principale.name,
+            'url_calculee': hotel.photo_principale.url,
+            'storage_du_champ': hotel.photo_principale.storage.__class__.__module__ + '.' + hotel.photo_principale.storage.__class__.__name__,
+        }
     return JsonResponse({
         'default_file_storage': getattr(settings, 'DEFAULT_FILE_STORAGE', None),
+        'storages_dict': getattr(settings, 'STORAGES', None),
         'cloud_name_defini': bool(settings.CLOUDINARY_STORAGE.get('CLOUD_NAME')),
         'cloud_name_longueur': len(settings.CLOUDINARY_STORAGE.get('CLOUD_NAME', '')),
+        'default_storage_class': default_storage.__class__.__module__ + '.' + default_storage.__class__.__name__,
+        'exemple_hotel': exemple,
     })
 
 
